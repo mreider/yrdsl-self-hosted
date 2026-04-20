19da82739b543c37e589126e20c5125f5f040172
Matt Reider <mreider@gmail.com>
2026-04-20 21:00:27 +0200

    Vendor refresh from yard-sale@91fe697 (#1)
    * vendor refresh from yard-sale@91fe697
    
    * fix: rewrite @yrdsl/core imports to relative paths

--- src/vendor/viewer/index.ts
+++ src/vendor/viewer/index.ts
@@ -1,4 +1,4 @@
 export { SaleViewer } from './SaleViewer.js';
 export type { SaleViewerProps } from './SaleViewer.js';
-// Re-export the canonical types so consumers don't have to pull in @yard-sale/core directly.
+// Re-export the canonical types so consumers don't have to pull in @yrdsl/core directly.
 export type { SaleSite, SaleItem, SaleAbout, SaleContact, ReservationInfo } from '../core/sale.js';
